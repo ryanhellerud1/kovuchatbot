e91a22d11f33af46b499f730568e69d27de642d3
ryanhellerud1 <ryan.hellerud@gmail.com>
2025-07-03 23:34:11 -0700

    db migrations

--- lib/db/migrations/0007_smiling_preak.sql
+++ lib/db/migrations/0007_smiling_preak.sql
@@ -1,1 +1,15 @@
-ALTER TABLE "document_chunks" ALTER COLUMN "embedding" SET DATA TYPE vector(1536);
+-- First create a temporary function to convert JSON to vector
+CREATE OR REPLACE FUNCTION json_to_vector(json_data json) RETURNS vector(1536) AS $$
+BEGIN
+  IF json_data IS NULL THEN
+    RETURN NULL;
+  END IF;
+  RETURN json_data::text::vector(1536);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Convert the column type
+ALTER TABLE "document_chunks" ALTER COLUMN "embedding" SET DATA TYPE vector(1536) USING json_to_vector(embedding);
+
+-- Drop the temporary function
+DROP FUNCTION json_to_vector(json);
